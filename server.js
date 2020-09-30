b13393b2cef89bf8babf1b2af2f3ec045934391b
Patipat Chanrungruang <patipat_cha@freewillsolutions.com>
2020-09-30 16:56:33 +0700

    1656

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const port = 3001
 
 const mosca = require('mosca')
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log('Ex'))
+app.get('/', (req, res) => res.send('Hello Soso'))
+app.listen(port, () => console.log('Connected'))
